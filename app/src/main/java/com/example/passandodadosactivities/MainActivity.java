90290160b2e0943cedf16243d1bd43f0fcfe3f3b
Rubens <rubao.ma@gmail.com>
2020-09-20 00:22:09 -0300

    first commit

---
+++ app/src/main/java/com/example/passandodadosactivities/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.passandodadosactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonEnviar;
+    private int idade = 40;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonEnviar = findViewById(R.id.buttonEnviar);
+
+        buttonEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(), SegundaActivity.class);
+
+                //passar dados
+                intent.putExtra("nome", "Rubens");
+                intent.putExtra("idade", idade);
+
+                startActivity(intent);
+
+            }
+        });
+    }
+}
